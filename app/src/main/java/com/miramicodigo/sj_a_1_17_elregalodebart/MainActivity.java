ddd717594dc0f7cb93a6e8f7d345742183adb075
Gustavo <lizarraga.gux@gmail.com>
2017-04-13 13:23:02 -0400

    Commit inicial

---
+++ app/src/main/java/com/miramicodigo/sj_a_1_17_elregalodebart/MainActivity.java
@@ -0,0 +1,74 @@
+package com.miramicodigo.sj_a_1_17_elregalodebart;
+
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SoundPool spIdiota, spCallate, spVeteAlDiablo;
+    private int resIdiota, resCallate, resVeteAlDiablo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        createSoundPool();
+    }
+
+    private void createSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createNewSoundPool();
+        } else {
+            createOldSoundPool();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createNewSoundPool(){
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        spIdiota = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        spCallate = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        spVeteAlDiablo = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        chargeSoundPool();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void createOldSoundPool(){
+        spIdiota = new SoundPool(15, AudioManager.STREAM_MUSIC,0);
+        spCallate = new SoundPool(10,AudioManager.STREAM_MUSIC,0);
+        spVeteAlDiablo = new SoundPool(12,AudioManager.STREAM_MUSIC,0);
+        chargeSoundPool();
+    }
+
+    public void chargeSoundPool() {
+
+    }
+
+    public void idiota(View v) {
+
+    }
+
+    public void callate(View v) {
+
+    }
+
+    public void veteAlDiablo(View v) {
+
+    }
+}
